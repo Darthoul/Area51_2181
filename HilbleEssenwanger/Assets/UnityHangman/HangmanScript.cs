267c66d038efdf1d37926bb08bad60e40b86059d
Darthoul <hessenrod@gmail.com>
2018-03-27 22:00:14 -0500

    Cree scripts en base al Proyecto de Visual Studio y objetos en escena para probarlo

---
+++ HilbleEssenwanger/Assets/UnityHangman/HangmanScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HangmanScript : MonoBehaviour {
+
+    public string palabra;
+    string palabraEscondida;
+
+    public Text outputText;
+    public InputField inputText;
+
+	// Use this for initialization
+	void Start () {
+        foreach (char c in palabra) {
+            palabraEscondida += "*";
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (outputText.text != palabraEscondida) {
+            outputText.text = palabraEscondida;
+        }
+        if (Input.GetKeyDown (KeyCode.Return)) {
+            string letra = inputText.text.Substring (0, 1);
+            if (palabra.Contains(letra)) {
+                string palabraTemporal = "";
+                for (int i = 0; i < palabra.Length; i++) {
+                    if (palabra[i] == letra[0]) {
+                        palabraTemporal += letra;
+                    } else {
+                        palabraTemporal += palabraEscondida[i];
+                    }
+                }
+                palabraEscondida = palabraTemporal;
+            }
+            inputText.text = "";
+        }
+        if (palabraEscondida == palabra) {
+            Debug.Log ("FELICIDADES!");
+        }
+	}
+}
