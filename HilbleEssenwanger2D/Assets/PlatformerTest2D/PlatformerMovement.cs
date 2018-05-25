93cf4dfa009402caf5b7ac4ccc0a9801aaba1607
Darthoul <hessenrod@gmail.com>
2018-05-24 22:10:45 -0500

    Hacemos checkeo de piso en el Update

--- HilbleEssenwanger2D/Assets/PlatformerTest2D/PlatformerMovement.cs
+++ HilbleEssenwanger2D/Assets/PlatformerTest2D/PlatformerMovement.cs
@@ -9,6 +9,9 @@ public class PlatformerMovement : MonoBehaviour {
     public float horizontalSpeed = 1;
     public float jumpForce = 1;
 
+    Vector3 leftNode { get { return transform.position - new Vector3 (0.5f, 1, 0); } }
+    Vector3 rightNode { get { return transform.position + new Vector3 (0.5f, -1, 0); } }
+
     bool isGrounded;
 	// Use this for initialization
 	void Start () {
@@ -17,6 +20,18 @@ public class PlatformerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        RaycastHit2D left = Physics2D.Raycast (leftNode, Vector3.down, 0.1f);
+        RaycastHit2D right = Physics2D.Raycast (rightNode, Vector3.down, 0.1f);
+
+        if (left || right) {
+            isGrounded = true;
+            verticalSpeed = 0;
+            CheckReposition (new RaycastHit2D[] { left, right });
+        } else { 
+            isGrounded = false; 
+        }
+
         if (!isGrounded) {
             verticalSpeed -= gravity * Time.deltaTime;
         } else {
@@ -25,23 +40,24 @@ public class PlatformerMovement : MonoBehaviour {
                 isGrounded = false;
             }
         }
-
         transform.Translate (Input.GetAxis ("Horizontal") * horizontalSpeed * Time.deltaTime, verticalSpeed * Time.deltaTime, 0);
-        
-        Debug.DrawRay (transform.position, Vector3.down, Color.green);
 	}
 
-    void OnTriggerEnter2D (Collider2D other) {
-        if (other.CompareTag("Platform")) {
-            isGrounded = true;
-            verticalSpeed = 0;
-            RaycastHit2D hit2D = Physics2D.Raycast (transform.position, Vector3.down);
-            float currentDistance = 0;
-            if (hit2D != null) {
-                currentDistance = hit2D.distance;
+    void CheckReposition (RaycastHit2D[] nodeRays) {
+        foreach (RaycastHit2D ray in nodeRays) {
+            if (ray) {
+                float distance = ray.collider.transform.localScale.y / 2;
+                float difference = (leftNode.y - ray.collider.transform.position.y) - distance;
+                if (difference != 0) {
+                    transform.Translate (0, -difference, 0);
+                }
+                break;
             }
-            Debug.Log (currentDistance);
-            transform.Translate (0, 1 - currentDistance, 0);
         }
+    }
+
+    void OnDrawGizmos () {
+        Gizmos.DrawSphere (leftNode, 0.2f);
+        Gizmos.DrawSphere (rightNode, 0.2f);
     }
 }
