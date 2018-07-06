f186b51de6fcc787ec352990e982895ce5bc8be1
jtorriccelli1986 <jtorriccelli1986@gmail.com>
2018-07-05 22:08:41 -0500

    Casi terminamos con el prototipo3D

--- HilbleEssenwanger3D/Assets/FirstTest3D/Scripts/PowerBallBehaviour.cs
+++ HilbleEssenwanger3D/Assets/FirstTest3D/Scripts/PowerBallBehaviour.cs
@@ -6,6 +6,7 @@ public class PowerBallBehaviour : MonoBehaviour {
 
     public PlatformMovement3D activePlayer;
     public Collider triggerArea;
+    public Collider damageArea;
     public readonly string containerName = "PowerContainer";
     public readonly Vector3 idlePoint = new Vector3 (0.5f, 0.75f, 0f);
     public readonly Vector3 center = new Vector3 (0f, 0.75f, 0f);
@@ -38,10 +39,18 @@ public class PowerBallBehaviour : MonoBehaviour {
         waitForNextAction = true;
         transform.parent.localPosition = center;
         GetComponent<Animator> ().SetTrigger ("Roundabout");
+        damageArea.enabled = true;
     }
 
     public void ResetPoint () {
         transform.parent.localPosition = idlePoint;
         waitForNextAction = false;
+        damageArea.enabled = false;
+    }
+
+    void OnTriggerEnter (Collider other) {
+        if (other.CompareTag("Damageable")) {
+            other.GetComponent<EnemyObject> ().TakeDamage ();
+        }
     }
 }
